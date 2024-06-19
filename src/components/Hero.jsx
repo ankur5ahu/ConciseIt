b0ce19663cb558e79c48cd5cdf24ebfd0d3fbfff
ankur <ankursahuhp@gmail.com>
2024-06-20 02:35:01 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+      </nav>
+
+      <h1 className='head_text'>
+        Concise It
+      </h1>
+      <h2 className='desc'>
+        an article summarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
